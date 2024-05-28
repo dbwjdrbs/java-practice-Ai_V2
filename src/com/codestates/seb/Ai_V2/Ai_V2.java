023f8e31accd4206b624d1619d203a96f6e3a63b
dbwjdrbs <klolp2@naver.com>
2024-05-28 20:47:42 +0900

    유정균 java-pratice-Ai_v2

--- src/com/codestates/seb/Ai_V2/Ai_V2.java
+++ src/com/codestates/seb/Ai_V2/Ai_V2.java
@@ -15,20 +15,30 @@ public class Ai_V2 {
      * y      : 예측한 결과값(웹 페이지 방문자 수)
      * y_list :
      */
+    Scanner sc = new Scanner(System.in);
+    double a, b;
 
-    //TODO:
+    System.out.println("[인공지능 프로그램 V2]");
+    System.out.println("==============================");
+    System.out.print("[System] a 값을 입력하여 주세요 : ");
+    a = Integer.parseInt(sc.nextLine());
+    System.out.print("[System] b 값을 입력하여 주세요 : ");
+    b = Integer.parseInt(sc.nextLine());
+    System.out.println("==============================");
 
-    // 사용자의 입력을 받기 위한 Scanner 객체를 생성합니다.
-
-    // 시스템 시작 안내 문구
-
+    System.out.println("[안내] 입력된 '광고비'는 다음과 같습니다.");
     // 이전 버전과 달리 다수의 '광고비'를 '배열'로 정의합니다.
-
+    double advertisementCost[] = new double[]{580.0, 700.0, 810.0, 840.0};
     // 반복문을 활용하여 광고비 배열을 출력합니다.
+    for (int i = 0; i < advertisementCost.length; i++) {
+      // 한 줄 띄기 및 예측 결과 안내 문구 출력
+      System.out.printf("%d 번째) %.1f원 ", i + 1, advertisementCost[i]);
+    }
+    System.out.println("[안내] AI의 '웹 페이지 방문자' 예측 결과는 다음과 같습니다.");
 
-    // 한 줄 띄기 및 예측 결과 안내 문구 출력
-
+    for (int i = 0; i < advertisementCost.length; i++) {
+      System.out.printf("%d 번째 예측) %.1f회 방문 \n", i + 1, (a * advertisementCost[i]) + b);
+    }
     // 반복문을 활용해 다수의 광고비(x_list)를 호출하여 방문자 수 예측값을 연산합니다.
-
   }
 }
